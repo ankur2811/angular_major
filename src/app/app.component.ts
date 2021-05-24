422cbc9512c2b20d5c0bdf227484232714c0334e
Ankur Garg <ankurgarg3@deloitte.com>
2021-05-23 18:08:38 -0700

    add socket code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { WebSocketService } from './web-socket.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-new-angular-app';
+
+  constructor(private webSocketService:WebSocketService){
+
+  }
+
+  ngOnInit(){
+    this.webSocketService.listen('test event').subscribe((data)=>{
+      console.log(data);
+    })
+  }
 }
---
+++ src/app/web-socket.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
+import  {io} from 'socket.io-client';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebSocketService {
+
+  socket:any;
+  readonly url:string="ws://localhost:3000";
+  constructor() {
+    this.socket=io(this.url)
+  }
+ 
+
+  listen(eventName:string){
+    return new Observable((subscriber)=>{
+    this.socket.on(eventName,(data:any)=>{
+      subscriber.next(data);
+    })
+    })
+  }
+
+
+  emit(eventName:string,data:any){
+    this.socket.emit(eventName,data);
+
+  }
+}
